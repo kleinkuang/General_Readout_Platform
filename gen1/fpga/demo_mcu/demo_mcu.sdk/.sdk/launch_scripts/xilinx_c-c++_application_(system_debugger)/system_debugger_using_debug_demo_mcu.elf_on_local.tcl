c580a8ff2288580303c4db013073225ca7984107
kleinkuang <klein_kuang@163.com>
2021-11-11 12:54:51 +0000

    demo_mcu

--- gen1/fpga/demo_mcu/demo_mcu.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_demo_mcu.elf_on_local.tcl
+++ gen1/fpga/demo_mcu/demo_mcu.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_demo_mcu.elf_on_local.tcl
@@ -3,6 +3,6 @@ configparams mdm-detect-bscan-mask 2
 targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-HS1 210512180081"} -index 0
 rst -processor
 targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-HS1 210512180081"} -index 0
-dow F:/General_Platform/fpga/demo_mcu/demo_mcu.sdk/demo_mcu/Debug/demo_mcu.elf
+dow F:/General_Readout_Platform/gen1/fpga/demo_mcu/demo_mcu.sdk/demo_mcu/Debug/demo_mcu.elf
 targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-HS1 210512180081"} -index 0
 con
